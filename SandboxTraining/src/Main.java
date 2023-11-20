58118af644b1384725ad2eb394e936389295f8e6
Vinícius Boroto dos Anjos <viniciusdosanjos63@gmail.com>
2023-11-19 21:20:30 -0300

    Finalizando aula 49

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite a quantidade de números que serão somados:");
+		int max = sc.nextInt();
+		double soma = 0;
 		
-		
+		for ( int i = 0; i < max; i++) {
+			System.out.println("Insira um número para somar:");
+			soma += sc.nextDouble();
+		}
+		System.out.println();
+		System.out.println("Resultado: " + soma);
+		sc.close();
 	}
 
 }
