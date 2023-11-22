ae3aa9ef5dc8a0317cc40d81d775073f27554b15
Vinícius Boroto dos Anjos <viniciusdosanjos63@gmail.com>
2023-11-21 23:58:56 -0300

    finalizando aulas de encapsulacao

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -1,21 +1,34 @@
+
+import java.util.Locale;
 import java.util.Scanner;
+import entities.Product;
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite a quantidade de números que serão somados:");
-		int max = sc.nextInt();
-		double soma = 0;
-		
-		for ( int i = 0; i < max; i++) {
-			System.out.println("Insira um número para somar:");
-			soma += sc.nextDouble();
-		}
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
 		System.out.println();
-		System.out.println("Resultado: " + soma);
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		sc.close();
 	}
-
 }
