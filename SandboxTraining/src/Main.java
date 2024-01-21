cb88392e0d096ae18465aa08fbd2c27981f66de7
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-01-20 23:58:57 -0300

    more sandbox date training

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 	public static void main(String[] args) {
 
+		DateTimeFormatter yourFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate minhaData = LocalDate.parse("2024-01-20");
+		
+		
 	}
 }
