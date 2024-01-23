050bfbd1a0267ef04091195b6f435c9ad6cbacf1
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-01-22 23:46:30 -0300

    LeetCode palindrome number exercise test

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -6,8 +6,12 @@ import java.time.ZoneId;
 public class Main {
 	public static void main(String[] args) {
 
-		Instant d01 = Instant.parse("2023-01-22T23:04:00Z");
-		LocalDateTime ld01 = LocalDateTime.ofInstant(d01, ZoneId.systemDefault());
-		System.out.println(ld01);
+		int inputNum = -121;
+		String inputStr = String.valueOf(inputNum);
+		String[] charArray = inputStr.split("");
+		for (String s : charArray) {
+			System.out.println(s);
+		}
+		
 	}
 }
