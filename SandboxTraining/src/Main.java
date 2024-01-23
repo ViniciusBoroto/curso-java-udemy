6692610df64b100f4b6cd6410bd686ece697c25e
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-01-22 23:07:37 -0300

    Instant to local test

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -1,9 +1,13 @@
+import java.time.Instant;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("teste git");
+		Instant d01 = Instant.parse("2023-01-22T23:04:00Z");
+		LocalDateTime ld01 = LocalDateTime.ofInstant(d01, ZoneId.systemDefault());
+		System.out.println(ld01);
 	}
 }
