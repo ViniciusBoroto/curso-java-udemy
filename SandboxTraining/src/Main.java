b18406e573658fdf7a054298e307d0d824d7758b
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-01-22 22:54:13 -0300

    teste notebook eclipse

--- SandboxTraining/src/Main.java
+++ SandboxTraining/src/Main.java
@@ -4,9 +4,6 @@ import java.time.format.DateTimeFormatter;
 public class Main {
 	public static void main(String[] args) {
 
-		DateTimeFormatter yourFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate minhaData = LocalDate.parse("2024-01-20");
-		
-		
+		System.out.println("teste git");
 	}
 }
