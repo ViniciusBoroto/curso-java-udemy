1e080e2fb80be354607b8c47e72df7d0c781ec3b
Vinicius Boroto <viniciusdosanjos63@gmail.com>
2024-01-29 16:22:47 -0300

    Ex 162 start

---
+++ _162_Exercise/src/entities/ImportedProduct.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class ImportedProduct extends Product {
+
+	private double customsFee;
+	
+	public ImportedProduct(String name, double price, double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(double customsFee) {
+		this.customsFee = customsFee;
+	}
+
+	@Override
+	public String priceTag() {
+		return String.format("%s $ %.2f (Customes fee: $ %.2f)", name, totalPrice(), customsFee);
+	}
+	
+	public double totalPrice() {
+		return price + customsFee;
+	}
+}
